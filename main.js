5ceaa659869478e1f650cd0630bfb9240575c636
Miguel Fonseca <miguelcsf@gmail.com>
2011-10-30 20:03:36 +0000

    Incremental search: basic style; list filtering.

--- main.js
+++ main.js
@@ -7,14 +7,49 @@ function updateState(newState) {
     $("#body_" + state).show();
 };
 
+function incrSearchUpdate() {
+    var input = $("#incr_search").val().toLowerCase();
+
+    $("#list > li").each(function () {
+            if ($(this).text().toLowerCase().match(input)) {
+                $(this).animate({ opacity: 1 }, 500);
+                $(this).show();
+            }
+            else {
+                $(this).hide();
+            }
+    });
+};
+
+function incrSearchReset() {
+    var obj = $("#incr_search");
+    obj.val("Buscar...");
+    obj.css("color", "#D3D3D3");
+
+    $("#list > li").each(function() {
+        $(this).animate({ opacity: 0.2 }, 500);
+        $(this).show();
+    });
+};
+
+function incrSearchActivate() {
+    var obj = $("#incr_search");
+    obj.val("");
+    obj.css("color", $("body").css("color"));
+};
+
+
 $(document).ready(function() {
 
+        var timeout = null;
+
         updateState(state);
+        incrSearchReset($("#incr_search"));
 
         /**
          * Update header when selecting a menu item
          */
-        $(".nav_button.selectable").click(function(event) {
+        $(".nav_button.selectable").click(function() {
             /* Decorate selected menu item */
             $(this).siblings().removeClass("selected");
             $(this).addClass("selected");
@@ -26,4 +61,15 @@ $(document).ready(function() {
             updateState($(this).get(0).id.substr(4));
         });
 
+        /**
+         * Procurar > Objecto > Incremental search
+         */
+
+        $("#incr_search").keydown(function() {
+            if (timeout) clearTimeout(timeout);
+            timeout = setTimeout(function(){ incrSearchUpdate(); }, 100);
+        });
+
+        $("#incr_search").click(function() { incrSearchActivate(); });
+        $("#incr_search_clear").click(function() { incrSearchReset(); });
 });
