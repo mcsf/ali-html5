275c334b9a5178e016563ec8ae660f617b96a059
Miguel Fonseca <miguelcsf@gmail.com>
2011-10-31 01:19:39 +0000

    Inc. search: automatic room shadowing

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /* Globals */
 var canvas;
 var ctx;
+var house;
 var state = "object"; /* Initial state */
 
 
@@ -9,12 +10,21 @@ var objects = [
         icon        : "chaves_casa_icon.png",
         description : "Chaves da casa",
         location    : "Secretária do quarto",
+        room        : 0,
     },
     {
         icon        : "chaves_carro_icon.png",
         description : "Chaves do carro",
         location    : "Cómoda da despensa",
+        room        : 2,
     },
+];
+
+var rooms = [
+    [ [2,129], [263,2], [263,223], [2,223] ],
+    [ [2,225], [154,225], [154,487], [2,487] ],
+    [ [157,225], [263,225], [263,350], [157,350] ],
+    [ [157,353], [263,353], [263,487], [157,487] ],
 ];
 
 
@@ -26,18 +36,30 @@ function updateState(newState) {
 
 
 function incrSearchUpdate() {
-    var input = $("#incr_search").val().toLowerCase();
+    var input         = $("#incr_search").val().toLowerCase();
+    var gray          = "rgba(211, 211, 211, 0.8)";
+    var inactiveRooms = rooms.slice(0);
+
+    ctx.clearRect(0, 0, 270, 660);
+    drawHouse();
 
     $("#itemlist > div").each(function () {
-            //if ($(this).text().toLowerCase().match(input)) {
             if ($(this).find(".description").text().toLowerCase().match(input)) {
                 $(this).animate({ opacity: 1 }, 500);
                 $(this).show();
+
+                var roomNo = $(this).find(".roomNo").val();
+                inactiveRooms.splice(roomNo, 1);
             }
             else {
                 $(this).hide();
             }
     });
+
+    $.each(inactiveRooms, function(i,v) {
+        fillRoom({ roomValue: v, style: gray });
+    });
+
 };
 
 function incrSearchReset() {
@@ -49,6 +71,9 @@ function incrSearchReset() {
         $(this).animate({ opacity: 0.2 }, 500);
         $(this).show();
     });
+
+    ctx.clearRect(0, 0, 270, 660);
+    drawHouse();
 };
 
 function incrSearchActivate() {
@@ -59,20 +84,31 @@ function incrSearchActivate() {
 
 
 function drawHouse() {
-    var img = new Image();
-    img.src = "house.png";
-    img.onload = function() {
-        ctx.drawImage(img, 0, 0);
-        ctx.fillStyle = "rgba(200, 0, 0, 0.5)";
-        ctx.fillRect (18, 167, 15, 15);
-        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-        ctx.fillRect (233, 249, 15, 15);
-    };
+    ctx.drawImage(house, 0, 0);
+    ctx.fillStyle = "rgba(200, 0, 0, 0.5)";
+    ctx.fillRect (18, 167, 15, 15);
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect (233, 249, 15, 15);
+};
+
+function fillRoom(params) {
+    if (params.style) ctx.fillStyle = params.style;
+
+    var room;
+    if (params.roomNo) room = rooms[roomNo];
+    else if (params.roomValue) room = params.roomValue;
+
+    ctx.beginPath();
+    $.each(room, function(i,v) {
+        if (i == 0) ctx.moveTo(v[0], v[1]);
+        else        ctx.lineTo(v[0], v[1]);
+    });
+    ctx.fill();
 };
 
 
 function createItem(attrs) {
-    var item = $('<div class="item"> <span class="icon"> <img src="' + attrs.icon + '"/> </span> <div class="text"> <span class="description">' + attrs.description + '</span> <span class="Location">' + attrs.location + '</span> </div> </div>');
+    var item = $('<div class="item"> <span class="icon"> <img src="' + attrs.icon + '"/> </span> <div class="text"> <span class="description">' + attrs.description + '</span> <span class="Location">' + attrs.location + '</span> <input type="hidden" class="roomNo" value="' + attrs.room + '"/> </div> </div>');
     $("#itemlist").append(item);
 };
 
@@ -86,7 +122,9 @@ $(document).ready(function() {
         ctx     = canvas.getContext("2d");
         $("canvas").attr("width", "270").attr("height", "660");
 
-        drawHouse();
+        house        = new Image();
+        house.src    = "house.png";
+        house.onload = function() { drawHouse(); };
 
         $.each(objects, function(i,v) {
             createItem(v);
