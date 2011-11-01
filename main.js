90d4b792277898fca53a4d51d02af725fe6592b2
Miguel Fonseca <miguelcsf@gmail.com>
2011-11-01 17:45:16 +0000

    Clicking outside overlay closes (deletes) it.

--- main.js
+++ main.js
@@ -223,23 +223,39 @@ function createOverlay(id) {
     var o = $("#overlay_template").clone();
     var attrs = objects[id];
 
+    /* Fill out overlay with contents */
     o.find(".picture").attr("src", attrs.picture);
     o.find(".description").text(attrs.description);
     o.find(".location").append(attrs.location);
     o.find(".info").text(attrs.info);
 
+    /* Append it to page */
     o.appendTo("body").addClass("overlay").css("display", "inline");
+
+    /* Clicking on its close button deletes it */
     o.find(".close").click(function() { deleteOverlay(); });
 
+    /* Disable every clickable element that's not part of the new overlay */
     $(".selectable").not("input").not("#state *").css("cursor", "auto");
     $("input.selectable").attr("disabled", "true");
     $(".overlay .selectable").css("cursor", "pointer");
+
+    /* Clicking outside overlay deletes it */
+    setTimeout(function(){
+        $("*").bind('click', deleteOverlay);
+        $(".overlay").add(".overlay *")
+            .unbind('click', deleteOverlay)
+            .click(function(event){
+                event.stopPropagation();
+            });
+    }, 100);
 };
 
 function deleteOverlay() {
     $(".selectable").not("input").css("cursor", "pointer");
     $("input.selectable").removeAttr("disabled");
     $(".overlay").remove();
+    $("*").unbind('click', deleteOverlay);
 };
 
 
