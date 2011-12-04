52649f33a403560a1a529ea0416dacfa8ae7acde
Miguel Fonseca <miguelcsf@gmail.com>
2011-12-04 16:21:24 +0000

    Add checkreqs for browser requirements check

---
+++ checkreqs.js
@@ -0,0 +1,35 @@
+function setNope() {
+    $("#browser_nope").removeClass("hidden");
+    $("#browser_info").addClass("nope");
+};
+
+function setOK() {
+    $("#browser_ok").removeClass("hidden");
+    $("#browser_info").addClass("ok");
+}
+
+function setInfo(s) {
+    $("#browser_info")
+        .removeClass("hidden")
+        .text("O navegador detectado foi o "
+                + s + ".");
+};
+
+$(document).ready(function() {
+    var browser = jQuery.browser;
+
+    if (browser.mozilla) {
+        if (browser.version < 7.0) setNope();
+        else setOK();
+        setInfo("Mozilla Firefox, versão " + browser.version);
+    }
+
+    else if (browser.webkit) {
+        if (browser.version < 535.2) setNope();
+        else setOK();
+        setInfo("Chromium ou outro que usa Webkit, versão " + browser.version);
+    }
+    else {
+        setNope();
+    }
+});
